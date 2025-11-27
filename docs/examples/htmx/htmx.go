736f6e34807136a78779cc52c48170c089716b9f
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2025-11-27 00:46:39 -0800

    refactor: only use public fields/methods on TemplateData

--- docs/examples/htmx/htmx.go
+++ docs/examples/htmx/htmx.go
@@ -1,88 +1,77 @@
 package hypertext
 
 func (data *TemplateData[R, T]) HXLocation(link string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Location", link)
-	return data
+	return data.Header("HX-Location", link)
 }
 
 func (data *TemplateData[R, T]) HXPushURL(link string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Push-Url", link)
-	return data
+	return data.Header("HX-Push-Url", link)
 }
 
 func (data *TemplateData[R, T]) HXRedirect(link string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Redirect", link)
-	return data
+	return data.Header("HX-Redirect", link)
 }
 
 func (data *TemplateData[R, T]) HXRefresh() *TemplateData[R, T] {
-	data.response.Header().Set("HX-Refresh", "true")
-	return data
+	return data.Header("HX-Refresh", "true")
 }
 
 func (data *TemplateData[R, T]) HXReplaceURL(link string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Replace-Url", link)
-	return data
+	return data.Header("HX-Replace-Url", link)
 }
 
 func (data *TemplateData[R, T]) HXReswap(swap string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Reswap", swap)
-	return data
+	return data.Header("HX-Reswap", swap)
 }
 
 func (data *TemplateData[R, T]) HXRetarget(target string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Retarget", target)
-	return data
+	return data.Header("HX-Retarget", target)
 }
 
 func (data *TemplateData[R, T]) HXReselect(selector string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Reselect", selector)
-	return data
+	return data.Header("HX-Reselect", selector)
 }
 
 func (data *TemplateData[R, T]) HXTrigger(eventName string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Trigger", eventName)
-	return data
+	return data.Header("HX-Trigger", eventName)
 }
 
 func (data *TemplateData[R, T]) HXTriggerAfterSettle(eventName string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Trigger-After-Settle", eventName)
-	return data
+	return data.Header("HX-Trigger-After-Settle", eventName)
 }
 
 func (data *TemplateData[R, T]) HXTriggerAfterSwap(eventName string) *TemplateData[R, T] {
-	data.response.Header().Set("HX-Trigger-After-Swap", eventName)
-	return data
+	return data.Header("HX-Trigger-After-Swap", eventName)
 }
 
 func (data *TemplateData[R, T]) HXBoosted() bool {
-	return data.request.Header.Get("HX-Boosted") != ""
+	return data.Request().Header.Get("HX-Boosted") != ""
 }
 
 func (data *TemplateData[R, T]) HXCurrentURL() string {
-	return data.request.Header.Get("HX-Current-Url")
+	return data.Request().Header.Get("HX-Current-Url")
 }
 
 func (data *TemplateData[R, T]) HXHistoryRestoreRequest() bool {
-	return data.request.Header.Get("HX-History-Restore-Request") == "true"
+	return data.Request().Header.Get("HX-History-Restore-Request") == "true"
 }
 
 func (data *TemplateData[R, T]) HXPrompt() string {
-	return data.request.Header.Get("HX-Prompt")
+	return data.Request().Header.Get("HX-Prompt")
 }
 
 func (data *TemplateData[R, T]) HXRequest() bool {
-	return data.request.Header.Get("HX-Request") == "true"
+	return data.Request().Header.Get("HX-Request") == "true"
 }
 
 func (data *TemplateData[R, T]) HXTargetElementID() string {
-	return data.request.Header.Get("HX-Target")
+	return data.Request().Header.Get("HX-Target")
 }
 
 func (data *TemplateData[R, T]) HXTriggerName() string {
-	return data.request.Header.Get("HX-Trigger-Name")
+	return data.Request().Header.Get("HX-Trigger-Name")
 }
 
 func (data *TemplateData[R, T]) HXTriggerElementID() string {
-	return data.request.Header.Get("HX-Trigger")
+	return data.Request().Header.Get("HX-Trigger")
 }
